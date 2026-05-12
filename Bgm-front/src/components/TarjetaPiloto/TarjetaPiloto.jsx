e08e03264d8a2aff73dabd942a326cea55152409
BautiGayoso <Bautigayoso2001@gmail.com>
2026-05-12 16:48:26 -0300

    modificaciones del css y agregado de botones en las cartas de los pilotos

--- Bgm-front/src/components/TarjetaPiloto/TarjetaPiloto.jsx
+++ Bgm-front/src/components/TarjetaPiloto/TarjetaPiloto.jsx
@@ -3,19 +3,18 @@ import React from 'react'
 export const TarjetaPiloto = ({piloto}) => {
   return (
         <div
-  style={{backgroundColor: '#15151E',border: '3px solid #E10600',borderRadius: '18px',padding: '20px',width: '320px',fontFamily: 'sans-serif',
+  style={{backgroundColor: '#15151E',border: '2px solid #E10600',borderRadius: '10px',padding: '20px',width: '300px',fontFamily: 'sans-serif',
     boxShadow: '6px 6px 0px black',margin: '10px',display: 'flex',flexDirection: 'column',alignItems: 'center',color: 'white'
-  }}
->
+  }}>
   <p
     style={{margin: 0,fontSize: '12px',fontWeight: 'bold',color: '#AAA',alignSelf: 'flex-start'
     }}
   >
-    DRIVER ID: {piloto.driverId}
+    Piloto ID: {piloto.driverId}
   </p>
 
   <h1
-    style={{margin: '10px 0',fontSize: '26px',color: '#E10600',textTransform: 'uppercase',borderBottom: '2px solid #E10600',width: '100%',
+    style={{margin: '2px 0',fontSize: '26px',color: '#e61313',textTransform: 'uppercase',borderBottom: '2px solid #E10600',width: '100%',
         textAlign: 'center'
     }}
   >
@@ -23,21 +22,18 @@ export const TarjetaPiloto = ({piloto}) => {
   </h1>
 
   <img src="/pilots/albon.jpg" alt={piloto.familyName}
-    style={{width: '180px',height: '180px',objectFit: 'cover',borderRadius: '15px',border: '3px solid white',marginBottom: '15px'
+    style={{width: '150px',height: '150px',objectFit: 'cover',borderRadius: '15px',border: '1px solid white',marginBottom: '2px'
     }}
   />
-
   <h3
-    style={{fontSize: '16px',marginBottom: '8px',alignSelf: 'flex-start',color: '#E10600'
+    style={{fontSize: '16px',marginBottom: '4px',alignSelf: 'flex-start',color: '#E10600'
     }}
   >
     Información del piloto
   </h3>
-
   <div
-    style={{backgroundColor: 'rgba(255,255,255,0.08)',padding: '12px',borderRadius: '12px',border: '1px solid #444',width: '100%'
-    }}
-  >
+    style={{backgroundColor: 'rgba(255,255,255,0.08)',padding: '10px',borderRadius: '10px',border: '1px solid #444',width: '100%'
+    }}>
     <p style={{ margin: '6px 0', fontSize: '14px' }}>
       <strong>Número:</strong> #{piloto.permanentNumber}
     </p>
@@ -47,19 +43,52 @@ export const TarjetaPiloto = ({piloto}) => {
     <p style={{ margin: '6px 0', fontSize: '14px' }}>
       <strong>Nacionalidad:</strong> {piloto.nationality}
     </p>
-    <p style={{ margin: '6px 0', fontSize: '14px' }}>
+    {/* <p style={{ margin: '6px 0', fontSize: '14px' }}>
       <strong>Fecha de nacimiento:</strong> {piloto.dateOfBirth}
-    </p>
+    </p> */}
   </div>
-  <a
+  {/* <a
     href={piloto.url}
     target="_blank"
-    style={{marginTop: '18px',textDecoration: 'none',backgroundColor: '#E10600',color: 'white',padding: '10px 18px',borderRadius: '10px',
+    style={{marginTop: '10px',textDecoration: 'none',backgroundColor: '#8a7171',color: 'white',padding: '5px 10px',borderRadius: '10px',
         fontWeight: 'bold'
     }}
   >
     Ver piloto
-  </a>
+  </a> */}
+
+<div
+ style={{
+    display: 'flex',
+    gap: '10px',
+    marginTop: '18px'
+  }}
+>
+<button
+    style={{
+      backgroundColor: '#16a34a',
+      color: 'white',
+      border: 'none',
+      padding: '10px 16px',
+      borderRadius: '10px',
+      cursor: 'pointer'
+    }}
+  >
+    Agregar a equipo
+  </button>
+
+  <button
+    style={{
+      backgroundColor: '#dc2626',
+      color: 'white',
+      border: 'none',
+      padding: '10px 16px',
+      borderRadius: '10px',
+      cursor: 'pointer'
+    }}
+  >
+    Quitar del equipo</button>
+    </div>
 </div>
   )
 }
